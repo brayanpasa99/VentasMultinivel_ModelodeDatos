2cf37c2b57286ce3d4d341d049d21e0a471ed769
aeqyuntero <50630528+aeqyuntero@users.noreply.github.com>
2021-10-05 21:26:51 -0500

    Add files via upload

--- SQL/tablespace.sql
+++ SQL/tablespace.sql
@@ -31,44 +31,57 @@ CONNECT admproy;
 -- Password admproy nueva: bd2g3
 
 /*Creacion de roles*/
-CREATE ROLE consultor IDENTIFIED BY consultor;
+CREATE ROLE cliente IDENTIFIED BY cliente;
 CREATE ROLE gestor_c1 IDENTIFIED BY gestor_c1;
 CREATE ROLE gestor_c2 IDENTIFIED BY gestor_c2;
 CREATE ROLE supervisor IDENTIFIED BY admproy;
 
 -- Ejecutar ModeloRelacional.sql
 
-REVOKE all ON "RepresentantePeriodo" FROM consultor, gestor_c1, gestor_c2, supervisor;
-REVOKE all ON "Region" FROM consultor, gestor_c1, gestor_c2, supervisor;
-REVOKE all ON "Representante" FROM consultor, gestor_c1, gestor_c2, supervisor;
-REVOKE all ON "Periodo" FROM consultor, gestor_c1, gestor_c2, supervisor;
-REVOKE all ON "PedidoProducto" FROM consultor, gestor_c1, gestor_c2, supervisor;
-REVOKE all ON "Pedido" FROM consultor, gestor_c1, gestor_c2, supervisor;
-REVOKE all ON "Inventario" FROM consultor, gestor_c1, gestor_c2, supervisor;
-REVOKE all ON "Categoria" FROM consultor, gestor_c1, gestor_c2, supervisor;
-REVOKE all ON "Cliente" FROM consultor, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "RepresentantePeriodo" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Region" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Representante" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Periodo" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "PedidoProducto" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Pedido" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Inventario" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Categoria" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Cliente" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Pago" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Calificacion" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "RepresentantePedido" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "RepresentanteCliente" FROM cliente, gestor_c1, gestor_c2, supervisor;
+REVOKE all ON "Grado" FROM cliente, gestor_c1, gestor_c2, supervisor;
 
-GRANT select ON "Cliente" TO consultor, gestor_c1, gestor_c2, supervisor;
-GRANT select ON "Representante" TO consultor, gestor_c1, gestor_c2, supervisor;
+GRANT select ON "Cliente" TO cliente, gestor_c1, gestor_c2, supervisor;
+GRANT select ON "Representante" TO cliente, gestor_c1, gestor_c2, supervisor;
 GRANT select ON "Periodo" TO gestor_c2, supervisor;
-GRANT select ON "Pedido" TO consultor, gestor_c1, gestor_c2, supervisor;
+GRANT select ON "Pedido" TO cliente, gestor_c1, gestor_c2, supervisor;
 GRANT select ON "Producto" TO gestor_c1, gestor_c2, supervisor;
 GRANT select ON "Categoria" TO  gestor_c1, gestor_c2, supervisor;
 GRANT select ON "Region" TO gestor_c2, supervisor;
-GRANT select ON "PedidoProducto" TO consultor, gestor_c1, gestor_c2, supervisor;
+GRANT select ON "PedidoProducto" TO cliente, gestor_c1, gestor_c2, supervisor;
 GRANT select ON "RepresentantePeriodo" TO gestor_c2, supervisor;
-GRANT select ON "Inventario" TO consultor, gestor_c1, gestor_c2, supervisor;
+GRANT select ON "Inventario" TO cliente, gestor_c1, gestor_c2, supervisor;
+GRANT select ON "Pago" FROM cliente, gestor_c1, gestor_c2, supervisor;
+GRANT select ON "Calificacion" FROM gestor_c1, gestor_c2, supervisor;
+GRANT select ON "RepresentanteCliente" FROM gestor_c1, gestor_c2, supervisor;
+GRANT select ON "Grado" FROM gestor_c1, gestor_c2, supervisor;
 
 GRANT update ON "Cliente" TO gestor_c1, gestor_c2, supervisor;
 GRANT update ON "Representante" TO  gestor_c2, supervisor;
 GRANT update ON "Periodo" TO supervisor;
-GRANT update ON "Pedido" TO consultor, gestor_c1, gestor_c2, supervisor;
+GRANT update ON "Pedido" TO cliente, gestor_c1, gestor_c2, supervisor;
 GRANT update ON "Producto" TO supervisor;
 GRANT update ON "Categoria" TO supervisor;
 GRANT update ON "Region" TO supervisor;
 GRANT update ON "PedidoProducto" TO gestor_c1, gestor_c2, supervisor;
 GRANT update ON "RepresentantePeriodo" TO gestor_c2, supervisor;
 GRANT update ON "Inventario" TO supervisor;
+GRANT update ON "Pago" FROM cliente, gestor_c1, gestor_c2, supervisor;
+GRANT update ON "Calificacion" FROM cliente, supervisor;
+GRANT update ON "RepresentanteCliente" FROM cliente, gestor_c1, gestor_c2, supervisor;
+GRANT update ON "Grado" FROM supervisor;
 
 GRANT insert ON "Cliente" TO gestor_c1, gestor_c2, supervisor;
 GRANT insert ON "Representante" TO  gestor_c2, supervisor;
@@ -80,6 +93,10 @@ GRANT insert ON "Region" TO supervisor;
 GRANT insert ON "PedidoProducto" TO gestor_c1, gestor_c2, supervisor;
 GRANT insert ON "RepresentantePeriodo" TO gestor_c2, supervisor;
 GRANT insert ON "Inventario" TO supervisor;
+GRANT insert ON "Pago" FROM cliente, gestor_c1, gestor_c2, supervisor;
+GRANT insert ON "Calificacion" FROM cliente, supervisor;
+GRANT insert ON "RepresentanteCliente" FROM cliente, gestor_c1, gestor_c2, supervisor;
+GRANT insert ON "Grado" FROM supervisor;
 
 GRANT delete ON "Cliente" TO gestor_c1, gestor_c2, supervisor;
 GRANT delete ON "Representante" TO  gestor_c2, supervisor;
@@ -91,6 +108,10 @@ GRANT delete ON "Region" TO supervisor;
 GRANT delete ON "PedidoProducto" TO gestor_c1, gestor_c2, supervisor;
 GRANT delete ON "RepresentantePeriodo" TO gestor_c2, supervisor;
 GRANT delete ON "Inventario" TO supervisor;
+GRANT delete ON "Pago" FROM supervisor;
+GRANT delete ON "Calificacion" FROM supervisor;
+GRANT delete ON "RepresentanteCliente" FROM cliente, gestor_c1, gestor_c2, supervisor;
+GRANT delete ON "Grado" FROM supervisor;
 
 /*Manejo de objetos*/
 GRANT CREATE ANY INDEX TO gestor_c1, gestor_c2, supervisor;
@@ -107,7 +128,7 @@ GRANT SELECT ANY TABLE TO supervisor;
 GRANT INSERT ANY TABLE TO supervisor;
 GRANT DELETE ANY TABLE TO supervisor;
 GRANT ALTER SESSION TO supervisor;
-GRANT CREATE SESSION TO consultor, gestor_c1, gestor_c2, supervisor;
+GRANT CREATE SESSION TO cliente, gestor_c1, gestor_c2, supervisor;
 
 /*Gestion de la BD*/
 GRANT CREATE PROFILE TO supervisor;
@@ -128,7 +149,7 @@ GRANT DROP USER TO supervisor;
 GRANT ALTER DATABASE TO supervisor;
 /* GRANT ANY PRIVILEGE TO supervisor;
 GRANT ANY ROLE TO supervisor;
-GRANT UNLIMITED TABLESPACE TO consultor, gestor_c1, gestor_c2, supervisor; */
+GRANT UNLIMITED TABLESPACE TO cliente, gestor_c1, gestor_c2, supervisor; */
 
 /* Creacion de Usuarios crearusuario.sql*/
 
